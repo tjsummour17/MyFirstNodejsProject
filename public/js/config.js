4c02b4a186df5758d161d1603a4232ef5bec9a21
Talal Sammour <talalsammour@android-e374b061c3a90062.home>
2021-03-20 10:07:38 +0200

    add all files

---
+++ public/js/config.js
@@ -0,0 +1,3 @@
+turnConfig = {
+    iceServers: [{ urls: ["stun:eu-turn6.xirsys.com"] }, { username: "Ey7cD34rGkKje1bf5QXSsrT5GoiqHDIsCvErSl1HvN2KBSKLh5EZ1sqwExA_zsFNAAAAAGBF4ix0anN1bW1vdXIxNw==", credential: "736261d6-7fe9-11eb-a56b-0242ac140004", urls: ["turn:eu-turn6.xirsys.com:80?transport=udp", "turn:eu-turn6.xirsys.com:3478?transport=udp", "turn:eu-turn6.xirsys.com:80?transport=tcp", "turn:eu-turn6.xirsys.com:3478?transport=tcp", "turns:eu-turn6.xirsys.com:443?transport=tcp", "turns:eu-turn6.xirsys.com:5349?transport=tcp"] }]
+}
